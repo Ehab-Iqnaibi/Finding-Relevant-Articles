fd8085727cac85e270f8d32d22f32e60754f25a5
Ehab-Iqnaibi <iqnaibi_ehab@ppu.edu>
2022-10-18 23:21:17 +0300

    edit4

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # irrelevant_words function
 def iwords(text):
-    irrelevant_words = {".", ",",  " \n ", " the ", " a ", " and ","\n",' how '," ",' be ',' as ',' could ',
-                        " is ", " was ", " he ", " at ", " to ", " for ", " can ", " this ", " we ", " you "
-                        ," or ", " i ", " an "," but ", " so ", " yes ", " no ", " of " , '!',' thought '}
+    irrelevant_words = {".", ",",  " \n ", " the ", " a ", " and ","\n",' how '," ",' be ',' as ',' could ',' are ',
+                        " is ", " was ", " he ", " at ", " to ", " for ", " can ", " this ", " we ", " you ",' it ','many'
+                        ," or ", " i ", " an "," but ", " so ", " yes ", " no ", " of " , '!',' thought ',' that ',' him ',
+                        ' its '}
     for iw in irrelevant_words:
         text = text.replace(iw, " ")
-    print(500+i)
-    print('-----------')
-
+    #print('-----------1')
 
     return(text)
+
 # The number of repetitions of the word in the article
 def histogram(text):
     word_histogram = {
@@ -25,21 +25,35 @@ def histogram(text):
         word_histogram.pop("")
     except KeyError:
         pass
-    print(600+i)            #extra
-    print(word_histogram)
+    #print('---------2')       #extra
+    #print(word_histogram)
     return dict(list(sorted(word_histogram.items(), key=lambda kv: kv[1], reverse=True))[0:6])
-#def relevont_articles(text):
-    #    relevont_histogram = {
 
-    #    }
-    #      RW=0
-    #     for Rlist in key_word:
-    #         if word in Rlist:
-    #           RW = RW+1
-    #    if RW>=3
-    #    relevont_histogram[relevont_histogram] = word_histogram[word] + 1
+# key words of the articles
+def key_words(text):
+    keySet.clear()
+    for kw in text:
+        keySet.add(kw)
 
-#  return dict(list(sorted(word_histogram.items(), key=lambda kv: kv[1], reverse=True))[0:6])
+    print(keySet)  # dict
+    print('********5')
+    return(keySet)
+
+#Find most relevant” articles Two texts are relevant
+def relevont_articles(text_list,text_set,title):
+      relevont_histogram = {
+
+        }
+      j=0
+      for kl in text_list:
+          relevont_histogram[title[j]]=0
+          for ks in kl:
+              if ks in text_set:
+                 relevont_histogram[title[j]]=relevont_histogram[title[j]]+1
+
+          j=j+1
+
+      return dict(list(sorted(relevont_histogram.items(), key=lambda kv: kv[1], reverse=True))[0:3])
 
 
 # Articles files
@@ -47,26 +61,45 @@ def histogram(text):
 fNname=["story1","sport1",'sport2','sport3','sport4','sport5','sport5','sport7']
 n=len(fNname)
 fedit=[None]*n
+keySet=set()
+keyList=[None]*n
 
 i=0
-while i < 1:
+while i < n:
     F=str(fNname[i])+".txt"
-    print(F) #extra
+    print(F) #extra 'title of articles
     fOpen=open(F, 'r',errors='ignore')
-    print(fOpen.readable()) #extra
+    #print(fOpen.readable()) #extra
     fread=fOpen.read()
     fread=fread.lower()
-    print(fread) #extra
+    #print(fread) #extra
     fedit[i]=iwords(fread)
     word_list = fedit[i].split(" ") #Split the paragraph into words
-    print('*****')
-    print(word_list) #extra
+    #print(word_list) #extra
     whist=histogram(word_list)  # wlist fun.
-    print(whist)
-    print(400+i) #extra
+    #print('********3')  # extra
+    #print(whist)        # dict
+    #print('********4')
+    kwords=key_words(whist).copy()
+    keyList[i]=kwords
+    #    print(keyList[i])  # dict
+    #    print('********6')
     i=i+1
 
-
+print(keyList)
 print(fedit) #extra
 
-
+enter = 1
+while enter:
+    article=input('Enter your Article: ')
+    print('\n')
+    article=str(article).lower()
+    aedit=iwords(article)
+    aedit_list=aedit.split(" ")
+    ahist = histogram(aedit_list)
+    akeys=key_words(ahist).copy()
+    #Find most relevant” articles Two texts are relevant if the 6 mostly
+    #used words in each overlap at least for 3 words
+    arelevent=relevont_articles(keyList,akeys,fNname)
+    print(arelevent)
+    enter=0
